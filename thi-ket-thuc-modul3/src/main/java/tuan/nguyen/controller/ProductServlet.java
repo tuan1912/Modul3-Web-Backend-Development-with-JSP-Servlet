0810c243e9a482c01f6faa8e77b05a406b95f6c3
tuan1912 <tuan.nguyen191292@gmail.com>
2022-01-06 10:21:59 +0700

    fadsfa

--- thi-ket-thuc-modul3/src/main/java/tuan/nguyen/controller/ProductServlet.java
+++ thi-ket-thuc-modul3/src/main/java/tuan/nguyen/controller/ProductServlet.java
@@ -1,24 +1,46 @@
 package tuan.nguyen.controller;
 
 import tuan.nguyen.dao.ProductDAOImpl;
+import tuan.nguyen.model.Product;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "Product Servlet", urlPatterns = "/product")
 public class ProductServlet extends HttpServlet {
     ProductDAOImpl productDAO = new ProductDAOImpl();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+
     }
+
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String action = req.getParameter("action" );
+        if (action == null){
+            action = "";
+        }
+        switch (action){
+
+            default:
+                showListPage(req,resp);
+        }
     }
+
+    private void showListPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("product/list.jsp");
+        List<Product> products = productDAO.selectAllProducts();
+        req.setAttribute("products",products);
+        requestDispatcher.forward(req,resp);
+    }
+
+
 }
